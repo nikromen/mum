9d664261a226c340914f070bb6eff2f09cc16efa
Jiri Kyjovsky <j1.kyjovsky@gmail.com>
2022-11-27 21:42:27 +0100

    feat: Better command handling

--- mum/command.py
+++ mum/command.py
@@ -1,6 +1,7 @@
 from configparser import SectionProxy
 from datetime import date
 from itertools import zip_longest
+from typing import Callable, Any, Optional
 
 from rich.console import Console
 from rich.table import Table
@@ -8,6 +9,25 @@ from rich.table import Table
 from mum.constants import MAP_INT_TO_WEEKDAY
 from mum.enums import CommandEnum, Section
 from mum.todo_file import TodoFile
+
+
+MAP_COMMAND_TO_FUNC: dict[str, Callable[[Any], Any]] = {}
+
+
+def map_command(command: Optional[str | Callable] = None) -> Callable:
+    def _add_command_function(command_func: Callable) -> Callable:
+        if callable(command) or command is None:
+            command_name_ = command_func.__name__.strip("_")
+        else:
+            command_name_ = command
+
+        MAP_COMMAND_TO_FUNC[command_name_] = command_func
+        return command_func
+
+    if callable(command):
+        return _add_command_function(command)
+
+    return _add_command_function
 
 
 class Command:
@@ -30,13 +50,13 @@ class Command:
         for row in zip_longest(*result):
             table.add_row(*row)
 
-    def _ls(self, command: list[str], table: Table) -> bool:
-        if not command:
+    def _ls_logic(self, tail: list[str], table: Table) -> bool:
+        if not tail:
             self._ls_all(table)
             return True
 
         commands = [section.lower().capitalize() for section in Section]
-        fst_command = command[0].lower().capitalize()
+        fst_command = tail[0].lower().capitalize()
         if fst_command not in commands:
             return False
 
@@ -46,63 +66,67 @@ class Command:
             if fst_command == Section.todo.value.lower().capitalize():
                 table.add_row(f"{key}: {value}")
                 continue
-
             table.add_row(value)
 
         return True
 
-    def _rst(self) -> None:
+    @map_command
+    def _ls(self, tail: list[str]) -> bool:
+        table = Table()
+        if self._ls_logic(tail, table):
+            self._console.print(table)
+            return True
+
+        return False
+
+    @map_command
+    def _rst(self) -> bool:
         self._todo_file.bootstrap_todo_file()
+        return True
 
     @staticmethod
     def _max_section_number(section: SectionProxy) -> int:
         return int(max(sorted(section.keys()))) if section else 0
 
-    def _dn(self, text: str) -> bool:
-        if not text:
+    @map_command
+    def _dn(self, tail: list[str]) -> bool:
+        command_text = " ".join(tail)
+        if not command_text:
             return False
 
         day = MAP_INT_TO_WEEKDAY[date.today().weekday()]
         day_section = self._todo_file.get_section(Section[day])
-        day_section[str(self._max_section_number(day_section) + 1)] = text
+        day_section[str(self._max_section_number(day_section) + 1)] = command_text
+        self._todo_file.write_config_to_file()
         return True
 
-    def _td(self, command: list[str]) -> bool:
-        if not command:
+    @map_command
+    def _td(self, tail: list[str]) -> bool:
+        if not tail:
             return False
 
-        if command[0] == "dn":
-            if len(command) != 2 or not command[1].isnumeric():
+        if tail[0] == "dn":
+            if len(tail) != 2 or not tail[1].isnumeric():
                 return False
 
-            removed = self._todo_file.get_section(Section.todo).pop(command[1], None)
+            removed = (
+                self._todo_file.get_section(Section.todo).pop(tail[1], None).split(" ")
+            )
             return self._dn(removed)
 
         section = self._todo_file.get_section(Section.todo)
-        section[str(self._max_section_number(section) + 1)] = " ".join(command)
+        section[str(self._max_section_number(section) + 1)] = " ".join(tail)
+        self._todo_file.write_config_to_file()
         return True
 
     def run_command(self) -> bool:
         fst_command = self._command[0].lower()
+        command_func = MAP_COMMAND_TO_FUNC.get(fst_command)
+        if command_func is None:
+            return False
+
         tail = self._command[1 : len(self._command)]
-        if fst_command == CommandEnum.ls:
-            table = Table()
-            retval = self._ls(tail, table)
-            self._console.print(table)
-            return retval
+        if tail or fst_command == CommandEnum.ls:
+            return command_func(self, tail)  # type: ignore
 
-        if fst_command == CommandEnum.reset:
-            self._rst()
-            return True
-
-        if fst_command == CommandEnum.done:
-            retval = self._dn(" ".join(tail))
-            self._todo_file.write_config_to_file()
-            return retval
-
-        if fst_command == CommandEnum.todo:
-            retval = self._td(tail)
-            self._todo_file.write_config_to_file()
-            return retval
-
-        return False
+        return command_func(self)
